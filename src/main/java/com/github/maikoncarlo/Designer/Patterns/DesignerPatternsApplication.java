b884532bed3f51de66a7ca64a3a529c196fa427d
Maikoncarlos <maikonsaturno@gmail.com>
2023-10-07 21:54:45 -0300

    criando projeto Designer Pattern

---
+++ src/main/java/com/github/maikoncarlo/Designer/Patterns/DesignerPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlo.Designer.Patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignerPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignerPatternsApplication.class, args);
+	}
+
+}
